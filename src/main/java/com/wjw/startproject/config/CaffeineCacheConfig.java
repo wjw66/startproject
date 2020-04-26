e47ab64bf5ba2944ccce1da10604b6a7a57a0758
wjw <wjwjava01@163.com>
2020-04-27 01:13:25 +0800

    完善Controller

---
+++ src/main/java/com/wjw/startproject/config/CaffeineCacheConfig.java
@@ -0,0 +1,16 @@
+package com.wjw.startproject.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author : wjwjava01@163.com
+ * @date : 23:26 2020/4/26
+ * @description : 缓存管理器
+ */
+@Configuration
+@EnableCaching
+@Slf4j
+public class CaffeineCacheConfig {
+}
---
+++ src/main/java/com/wjw/startproject/interceptor/RateLimitInterceptor.java
@@ -0,0 +1,11 @@
+package com.wjw.startproject.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+/**
+ * @author : wjwjava01@163.com
+ * @date : 0:56 2020/4/27
+ * @description : 全局限流
+ */
+public class RateLimitInterceptor implements HandlerInterceptor {
+}
